4eb3ad62e9b6982472ff129e6d22878bff3e0a22
Mr_Gumppy <sherifbasosy@yahoo.com>
2022-05-08 19:47:56 +0200

    .

--- SuckCodeJam/Assets/Scripts/Player.cs
+++ SuckCodeJam/Assets/Scripts/Player.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //Publics
-    public float Speed;
-    public float Jump;
-    public LayerMask layermask;
+        //floats
+        public float Speed;
+        public float Jump;
+
+        //LayerMasks
+        public LayerMask layermask;
 
     //Privates
     Rigidbody2D rb;
@@ -17,24 +20,37 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Decleration
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Grounded = Physics2D.OverlapBox(transform.position , Vector2.one ,0 , layermask);
+
+        Movement();
+
+        
+    }
+
+    void Movement()
+    {
+        if (!rb)
+        {
+            return;
+        }
 
         float movex = Input.GetAxis("Horizontal");
         movement = new Vector2(movex * Speed, 0);
 
+        Grounded = Physics2D.OverlapBox(transform.position, Vector2.one, 0, layermask);
         if (Input.GetKeyDown(KeyCode.Space) && Grounded)
         {
-            Debug.Log("grounded");
-            movement = new Vector2(0 , Jump);
+            movement = new Vector2(0,Jump );
         }
 
-        rb.position = (rb.position +movement * Time.deltaTime);
+        rb.position = (rb.position + movement * Time.deltaTime);
     }
 
 }
